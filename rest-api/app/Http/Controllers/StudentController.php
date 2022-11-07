9525785ed448d1848c3f9392c99ad8eeffdfb242
Bobogeng <suirito@gmail.com>
2022-11-07 18:35:12 +0700

    Add Condition

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -9,56 +9,101 @@ class StudentController extends Controller
 {
     public function index() {
         $students = Student::all();
-        $data = [
-            'message' => 'Get all students',
-            'data' => $students
-        ];
-        return response()->json($data, 200);
+        if (!$students->isEmpty()) {
+            $data = [
+                'message' => 'Get all students',
+                'data' => $students
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Students not found',
+            ];
+            return response()->json($data, 404);
+        }
+    }
+    public function show($id) {
+        $student = Student::find($id);
+        if ($student) {
+            $data = [
+                'message' => 'Get detail student',
+                'data' => $student
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+            return response()->json($data, 404);
+        }
     }
     public function store(Request $request) {
-        $input = [
+        $student = [
             'nama' => $request->nama,
             'nim' => $request->nim,
             'email' => $request->email,
             'jurusan' => $request->jurusan,
         ];
 
-        $student = Student::create($input);
+        foreach ($student as $key => $value) {
+            if (!$key || !$value) {
+                $data = [
+                    'message' => $key . ' is required',
+                ];
+                
+                return response()->json($data, 404);
+            }
+        }
 
         $data = [
             'message' => 'Student is created successfully',
-            'data' => $student,
+            'data' => Student::create($student),
         ];
 
         return response()->json($data, 201);
     }
     public function update(Request $request, $id) {
-        $input = [
-            'nama' => $request->nama,
-            'nim' => $request->nim,
-            'email' => $request->email,
-            'jurusan' => $request->jurusan,
-        ];
-
-        Student::find($id)->update($input);
         $student = Student::find($id);
-
-        $data = [
-            'message' => 'Student is updated successfully',
-            'data' => $student,
-        ];
-
-        return response()->json($data, 200);
+        if ($student) {
+            $input = [
+                'nama' => $request->nama ?? $student->nama,
+                'nim' => $request->nim ?? $student->nim,
+                'email' => $request->email ?? $student->email,
+                'jurusan' => $request->jurusan ?? $student->jurusan,
+            ];
+    
+            $student->update($input);
+    
+            $data = [
+                'message' => 'Student is updated successfully',
+                'data' => $student,
+            ];
+    
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+    
+            return response()->json($data, 404);
+        }
     }
     public function destroy($id) {
-        Student::destroy($id);
-        $student = Student::all();
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
 
-        $data = [
-            'message' => 'Student is deleted successfully',
-            'data' => $student,
-        ];
-
-        return response()->json($data, 200);
+            $data = [
+                'message' => 'Student is deleted successfully',
+            ];
+    
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+    
+            return response()->json($data, 404);
+        }
     }
 }
