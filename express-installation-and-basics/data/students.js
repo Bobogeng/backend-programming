03e9846f48475aa6ecfe9f3b6e26c5a20fedd65f
Bobogeng <suirito@gmail.com>
2022-12-12 11:04:14 +0700

    Express Installation and Basics

---
+++ express-installation-and-basics/data/students.js
@@ -0,0 +1,29 @@
+const students = [
+    {
+        nama: "Irsal Fathi Farhat",
+        rombel: "TI06",
+        nim: "0110221146",
+    },
+    {
+        nama: "Muhammad Fahriza",
+        rombel: "TI06",
+        nim: "0110221147",
+    },
+    {
+        nama: "Muhammad Salsabil",
+        rombel: "TI07",
+        nim: "0110221178",
+    },
+    {
+        nama: "Muhammad Farhan Abdillah",
+        rombel: "TI07",
+        nim: "0110221175",
+    },
+    {
+        nama: "Ridho Fauzi Grafika",
+        rombel: "TI06",
+        nim: "0110221142",
+    },
+];
+
+module.exports = students;
